4d190ee6635b2ec340af77ebbd513f213c09a4b9
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-11 01:11:25 -0500

    revert test

--- tests/test_ticketsnek.py
+++ tests/test_ticketsnek.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_deploy(accounts, TicketSnek):
     snek = accounts[0].deploy(TicketSnek, "My new event!")
     assert snek.name() == "My new event!"
@@ -7,3 +9,6 @@ def test_buy_tickets(accounts, TicketSnek):
     for i, a in enumerate(accounts[:5]):
         snek.buy({'from': a})
         assert snek.tickets(i) == a
+
+    with pytest.reverts("Index out of range"):
+        snek.buy({'from': accounts[5]})
