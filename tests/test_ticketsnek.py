4f8178b51108d322fa658184013c6730ccc3d18f
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-11 01:50:50 -0500

    refactor to fixture

--- tests/test_ticketsnek.py
+++ tests/test_ticketsnek.py
@@ -6,27 +6,36 @@ def test_deploy(accounts, TicketSnek):
     assert snek.number_of_tickets() == 5
     assert snek.price() == 100
 
-def test_buy_tickets(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!", 7, 100)
-    for i, a in enumerate(accounts[:7]):
+
+@pytest.fixture(scope='module', autouse=True)
+def snek(accounts, TicketSnek):
+    yield accounts[0].deploy(TicketSnek, "My new event!", 5, 100)
+
+
+@pytest.fixture(autouse=True)
+def isolation(fn_isolation):
+    pass
+
+def test_buy_tickets(accounts, snek):
+    for i, a in enumerate(accounts[:snek.number_of_tickets()]):
         before_funds = snek.balance()
         snek.buy({'from': a, 'value': snek.price()})
         assert snek.has_ticket(a)
         assert snek.balance() - before_funds == snek.price()
 
-    with pytest.reverts("dev: All tickets sold!"):
-        snek.buy({'from': accounts[7]})
+def test_overbuy(accounts, snek):
+    test_buy_tickets(accounts, snek)  # Start from end of this test
 
-def test_cannot_buy_two_tickets(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!", 5, 100)
+    with pytest.reverts("dev: All tickets sold!"):
+        snek.buy({'from': accounts[snek.number_of_tickets()]})
+
+def test_cannot_buy_two_tickets(accounts, snek):
     snek.buy({'from': accounts[0]})
     with pytest.reverts("dev: You already bought a ticket!"):
         snek.buy({'from': accounts[0]})
 
-def test_withdraw_money(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!", 5, 100)
-    for a in accounts[:5]:
-        snek.buy({'from': a, 'value': snek.price()})
+def test_withdraw_money(accounts, snek):
+    test_buy_tickets(accounts, snek)  # Start from end of this test
 
     assert snek.balance() == 5 * snek.price()
     snek.withdraw({'from': accounts[5]})
