4fb098cfe7fba0c40d055b9b555a0d67df902d69
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-11 01:16:14 -0500

    set number of tickets at deployment

--- contracts/TicketSnek.vy
+++ contracts/TicketSnek.vy
@@ -2,16 +2,17 @@
 # 1. Create an event
 # 2. Someone can buy a ticket
 
-NUMBER_OF_TICKETS: constant(uint256) = 7
 
 name: public(string[64])
-tickets: public(address[NUMBER_OF_TICKETS])
+tickets: public(map(uint256, address))
+number_of_tickets: public(uint256)
 last_ticket_sold: uint256
 
 
 @public
-def __init__(_name: string[64]):
+def __init__(_name: string[64], _tickets: uint256):
     self.name = _name
+    self.number_of_tickets = _tickets
     self.last_ticket_sold = 0
 
 
--- tests/test_ticketsnek.py
+++ tests/test_ticketsnek.py
@@ -1,11 +1,12 @@
 import pytest
 
 def test_deploy(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!")
+    snek = accounts[0].deploy(TicketSnek, "My new event!", 5)
     assert snek.name() == "My new event!"
+    assert snek.number_of_tickets() == 5
 
 def test_buy_tickets(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!")
+    snek = accounts[0].deploy(TicketSnek, "My new event!", 7)
     for i, a in enumerate(accounts[:7]):
         snek.buy({'from': a})
         assert snek.tickets(i) == a
