e050133a99b5a08ed9855aed385204f3e1c5fdc7
Bryant Eisenbach <fubuloubu@gmail.com>
2020-01-11 01:35:30 -0500

    pay for tickets

--- contracts/TicketSnek.vy
+++ contracts/TicketSnek.vy
@@ -2,6 +2,7 @@
 # 1. Create an event
 # 2. Someone can buy a ticket
 # 3. There are a limited number of tickets
+# 4. Each ticket has a specific price
 
 
 name: public(string[64])
@@ -9,15 +10,19 @@ tickets: public(map(uint256, address))
 number_of_tickets: public(uint256)
 last_ticket_sold: uint256
 
+price: public(uint256(wei))
+
 
 @public
-def __init__(_name: string[64], _tickets: uint256):
+def __init__(_name: string[64], _tickets: uint256, _price: uint256(wei)):
     self.name = _name
     self.number_of_tickets = _tickets
     self.last_ticket_sold = 0
+    self.price = _price
 
 
 @public
+@payable
 def buy():
     assert self.last_ticket_sold < self.number_of_tickets  # dev: All tickets sold!
     self.tickets[self.last_ticket_sold] = msg.sender
--- tests/test_ticketsnek.py
+++ tests/test_ticketsnek.py
@@ -1,15 +1,18 @@
 import pytest
 
 def test_deploy(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!", 5)
+    snek = accounts[0].deploy(TicketSnek, "My new event!", 5, 100)
     assert snek.name() == "My new event!"
     assert snek.number_of_tickets() == 5
+    assert snek.price() == 100
 
 def test_buy_tickets(accounts, TicketSnek):
-    snek = accounts[0].deploy(TicketSnek, "My new event!", 7)
+    snek = accounts[0].deploy(TicketSnek, "My new event!", 7, 100)
     for i, a in enumerate(accounts[:7]):
-        snek.buy({'from': a})
+        before_funds = snek.balance()
+        snek.buy({'from': a, 'value': snek.price()})
         assert snek.tickets(i) == a
+        assert snek.balance() - before_funds == snek.price()
 
     with pytest.reverts("dev: All tickets sold!"):
         snek.buy({'from': accounts[7]})
